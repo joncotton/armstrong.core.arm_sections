0578fa5deca05af3a4c7f4555eda6bbe9e26cb78
Jon Cotton <jcotton@baycitizen.org>
2012-01-05 16:48:34 -0800

    Changes necessary for primary_sections in Arm_Content.
    Since the one Content Model now relates to Sections in two ways we need to find all the multiple Relation fields. The backend lookup goes through all related objects picking out the proper fields for all connections from the model represented by settings.ARMSTRONG_SECTION_ITEM_MODEL. Q objects handle the OR QuerySet filter.

--- armstrong/core/arm_sections/backends.py
+++ armstrong/core/arm_sections/backends.py
@@ -1,18 +1,20 @@
 from django.conf import settings
-
+from django.db.models import Q
 
 def find_related_models(section):
     rel = None
+    filters = None
     relateds = section._meta.get_all_related_objects() + \
                section._meta.get_all_related_many_to_many_objects()
     for related in relateds:
         found = "%s.%s" % (related.model.__module__,
                 related.model.__name__)
         if found == settings.ARMSTRONG_SECTION_ITEM_MODEL:
-            rel = related
-            break
-    kwargs = {rel.field.name: section}
-    qs = rel.model.objects.filter(**kwargs)
+            if not rel: rel = related
+            q = Q( **{related.field.name: section} )
+            filters = filters | q if filters else q
+
+    qs = rel.model.objects.filter(filters)
     if hasattr(qs, 'select_subclasses'):
         qs = qs.select_subclasses()
     return qs
