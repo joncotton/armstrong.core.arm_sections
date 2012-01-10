e2f28f5bb444361d5e7cae8d1d5c71741f43ca12
Jon Cotton <jcotton@baycitizen.org>
2012-01-09 17:16:09 -0800

    Revert "update SectionSlugManager to find a Story by either the FK or M2M Section Relation fields."
    This reverts commit e51a643ab9044d7f43ba23c9f959db604cf2fcc4.

--- armstrong/core/arm_sections/managers.py
+++ armstrong/core/arm_sections/managers.py
@@ -1,15 +1,10 @@
 from django.db import models
-from django.db.models import Q
 
 
 class SectionSlugManager(models.Manager):
-    def __init__(self,
-                 primary_section_field="primary_section",
-                 section_field="sections",
-                 slug_field="slug",
-                 *args, **kwargs):
+    def __init__(self, section_field="primary_section", slug_field="slug",
+            *args, **kwargs):
         super(SectionSlugManager, self).__init__(*args, **kwargs)
-        self.primary_section_field = primary_section_field
         self.section_field = section_field
         self.slug_field = slug_field
 
@@ -20,13 +15,11 @@ class SectionSlugManager(models.Manager):
             slug = slug[1:]
         section_slug, content_slug = slug.rsplit("/", 1)
         section_slug += "/"
-
-        primary = {self.slug_field: content_slug}
-        secondary = primary.copy()
-        primary["%s__full_slug" % self.primary_section_field] = section_slug
-        secondary["%s__full_slug" % self.section_field] = section_slug
-
-        qs = self.model.objects.filter(Q(**primary) | Q(**secondary))
+        kwargs = {
+                "%s__full_slug" % self.section_field: section_slug,
+                self.slug_field: content_slug,
+        }
+        qs = self.model.objects.filter(**kwargs)
         if hasattr(qs, "select_subclasses"):
             qs = qs.select_subclasses()
         try:
